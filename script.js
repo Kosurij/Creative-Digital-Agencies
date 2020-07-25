9870fe855e54c3f3e5c62f094724c217f2b44832
Yuriy Kosenko <62155518+Kosurij@users.noreply.github.com>
2020-07-25 13:54:11 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+$(function() {
+
+/*--- Fixed Header ---*/
+    let header = $('#header');
+    let intro = $('#intro');
+    let introH = intro.innerHeight();
+    let scrollPos = $(window).scrollTop();
+    let nav = $('#nav');
+    let toggleClass = $('#navToggle')
+
+    checkScroll(scrollPos,introH);
+
+    $(window).on("scroll resize" , function() {
+        introH = intro.innerHeight();
+        scrollPos = $(this).scrollTop();
+
+        checkScroll(scrollPos,introH);
+
+    })
+
+    function checkScroll(scrollPos, introH) {
+
+        if (scrollPos > introH) {
+            header.addClass('fixed');
+        } else {
+            header.removeClass('fixed');
+        }
+    }
+
+
+/*--- Smooth Scroll ---*/
+
+$('[data-scroll]').on('click', function(event){
+    event.preventDefault();
+
+    let elementId = $(this).data('scroll');
+    let elementOffset = $(elementId).offset().top;
+
+    nav.removeClass('show');
+
+    $('html, body').animate({
+        scrollTop: elementOffset - 60
+    }, 700);
+
+});
+
+/*--- Nav Toggle ---*/
+$('#navToggle').on('click', function(event) {
+    event.preventDefault();
+
+    nav.toggleClass('show')
+});
+
+});
